67344976e80b3a82806e2c8d93b3dbb03a66adea
Teddy Kalp <teddy.kalp@gmail.com>
2019-11-05 11:27:13 -0700

    Created pipeline for training and testing models as well as uploaded datasets with predictions scores

--- BugZ/TKnegsampleGeneration/utilities.py
+++ BugZ/TKnegsampleGeneration/utilities.py
@@ -1,13 +1,44 @@
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+import numpy as np
 
-def train_test(x1_df,x2_df):
+def train_test_binary_class(x1_df,x2_df):
+    x1_df = x1_df.sample(frac=1)
+    x2_df = x2_df.sample(frac=1)
     train_x = x1_df.drop(columns = ["BinaryClass","MultiClass"])
     train_y = x1_df["BinaryClass"]
     test_x = x2_df.drop(columns = ["BinaryClass","MultiClass"])
     test_y = x2_df["BinaryClass"]
     return train_x, train_y, test_x, test_y
 
+def x_y_split(df):
+    df = df.sample(frac=1)
+    train_x = df.drop(columns = ["BinaryClass","MultiClass"])
+    train_y = df["BinaryClass"]
+    return train_x, train_y
+
+
+def train_test_multi_class(x1_df,x2_df):
+    x1_df = x1_df.sample(frac=1)
+    x2_df = x2_df.sample(frac=1)
+    train_x = x1_df.drop(columns = ["BinaryClass","MultiClass"])
+    train_y = x1_df["MultiClass"]
+    train_y = train_y.replace("requires", 3)
+    train_y = train_y.replace("similar", 4)
+    test_x = x2_df.drop(columns = ["BinaryClass","MultiClass"])
+    test_y = x2_df["MultiClass"]
+    test_y = test_y.replace("requires", 3)
+    test_y = test_y.replace("similar", 4)
+    return train_x, train_y, test_x, test_y
+
+def x_y_multiclass_split(df):
+    train_df = df.sample(frac=1)
+    train_x = train_df.drop(columns = ["BinaryClass","MultiClass"])
+    train_y = train_df["MultiClass"]
+    train_y = train_y.replace("requires", 3)
+    train_y = train_y.replace("similar", 4)
+    
+    return train_x, train_y
 
 def split_into_lemmas(text):
     text = str(text)
@@ -19,3 +50,11 @@ def create_vectorizor():
     return CountVectorizer(tokenizer=lambda doc: doc, analyzer=split_into_lemmas, lowercase=False, stop_words='english')
 
 
+def create_classified_sets(train_x, test_x):
+    tfidf_transformer = TfidfTransformer()
+    count_vect = create_vectorizor()
+    X_train_counts = count_vect.fit_transform(np.array(train_x))
+    X_train_tfidf= tfidf_transformer.fit_transform(X_train_counts)
+    X_test_counts = count_vect.transform(np.array(test_x))
+    X_test_tfidf= tfidf_transformer.fit_transform(X_test_counts)
+    return X_train_tfidf, X_test_tfidf
